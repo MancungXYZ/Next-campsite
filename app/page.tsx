dea7e6fbe2f8eae9b5ce24b34fab6c8818bccd22
Mancung <60146927+MancungXYZ@users.noreply.github.com>
2024-01-29 22:10:07 +0700

    Hero dan camp pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Hero from "@/components/Hero";
 import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 
 export default function Home() {
@@ -7,6 +10,9 @@ export default function Home() {
     <>
       <Hero />
       <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
     </>
   )
 }
